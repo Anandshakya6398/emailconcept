9bcf6f53028bf72f95a293bbe010a357302cfe44
anandshakya6398 <prabhatshakya166@gmail.com>
2025-07-07 12:35:39 +0530

    sample to Generate 6-digit OTP

--- otpGenerate.js
+++ otpGenerate.js
@@ -1,4 +1,4 @@
 function otpGenerator() {
-    return Math.floor(100000 + Math.random() * 900000).toString();
+    return Math.floor((Math.random() * 10000) + 90000);
 }
 console.log(otpGenerator()); // Example usage, prints a 6-digit OTP
